f7e6e9495919a662505ad27569ab1397c64f313a
skcruzer <96440269+skcruzer@users.noreply.github.com>
2022-03-19 09:58:55 -0700

    separated routes to their own .js files, updates

--- server.js
+++ server.js
@@ -1,27 +1,21 @@
 const express = require('express')
-const path = require('path')
+const htmlRoutes = require('./routes/html-routes')
+const apiRoutes = require('./routes/api-routes')
 
 // call express as a function
 const app = express()
+
+const ports = process.env.PORT || 3000
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 // use html files located in public folder
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static('public'))
 
-
-app.get('/', (req, res) => {
-  // index.html is the response retrieved when a user goes to website
-  res.sendFile(path.join(__dirname, 'public', 'index.html'))
-})
-
-app.get('/notes', (req, res) => {
-  // notes.html is the response retrieved when /notes is requested
-  res.sendFile(path.join(__dirname, 'public', 'notes.html'))
-})
-
+app.use('/', htmlRoutes)
+app.use('/api', apiRoutes)
 
 
 // running on a port which is local host 3000
-app.listen(process.env.PORT || 3000)
+app.listen(ports)
