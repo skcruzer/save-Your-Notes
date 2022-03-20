60497297e12eb6afc4901c4c5dd3261b77142bc5
skcruzer <96440269+skcruzer@users.noreply.github.com>
2022-03-19 22:10:57 -0700

    previous saved notes, new notes/save new note

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const path = require('path')
+const notes = require('./db/db.json')
 
 // call express as a function
 const app = express()
@@ -21,7 +22,21 @@ app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'notes.html'))
 })
 
+app.get('/api/notes', (req, res) => {
+  // previous saved notes are retrieved
+  res.json(notes)
+})
 
+app.post('/api/notes', (req, res) => {
+  // user will receive a new note to save
+  let newNote = {
+    title: req.body.title,
+    text: req.body.text
+  }
+  // this will add user's note to the db.json file
+  notes.push(newNote)
+  res.json(200)
+})
 
 // running on a port which is local host 3000
 app.listen(process.env.PORT || 3000)
